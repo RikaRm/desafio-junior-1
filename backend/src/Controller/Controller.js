d9806ab2b100e6c14892a648ee2dc10599047cac
ricardo <ricardo@COMPRASSERVER.CORP>
2023-12-27 09:25:48 -0300

    controllers em contrução

---
+++ backend/src/Controller/Controller.js
@@ -0,0 +1,30 @@
+const useCase = () => { } // require("")
+const validation = () => { } // require("")
+class Controller {
+    async get(httpRequest) {
+        const user = await useCase(httpRequest.params.id)
+        return user
+    }
+
+    async getAll(httpRequest) {
+        const user = await useCase(httpRequest.query)
+        return user
+    }
+
+    async create(httpRequest) {
+        const user = await useCase(httpRequest.body)
+        return user
+    }
+
+    async update(httpRequest) {
+        const user = await useCase(httpRequest.params.id, httpRequest.body)
+        return user
+    }
+
+    async remove(httpRequest) {
+        const user = await useCase(httpRequest.params.id)
+        return user
+    }
+}
+
+module.exports = new Controller()
